b67e20e5d2888bfb44a301fc7e73b974e293a7a3
hugoiguana <hugo.iguanaa@gmail.com>
2019-05-09 17:08:43 -0300

    Criação do projeto.

---
+++ src/main/java/curso/udemy/spring/angular/ionc/LocalAplication.java
@@ -0,0 +1,13 @@
+package curso.udemy.spring.angular.ionc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalAplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalAplication.class, args);
+    }
+
+}
