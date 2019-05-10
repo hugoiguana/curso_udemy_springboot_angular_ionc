b7dd1c4d61d7a34dc050166fdca0305c01d4786d
hugoiguana <hugo.iguanaa@gmail.com>
2019-05-09 22:19:29 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/curso/udemy/spring/angular/ionc/domain/Categoria.java
+++ src/main/java/curso/udemy/spring/angular/ionc/domain/Categoria.java
@@ -6,16 +6,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
 
     private String nome;
