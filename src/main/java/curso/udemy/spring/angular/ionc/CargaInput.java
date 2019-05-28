d80dbea4633becb6b3538245a6b85ecf702800ad
hugoiguana <hugo.iguanaa@gmail.com>
2019-05-27 22:26:55 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/curso/udemy/spring/angular/ionc/CargaInput.java
+++ src/main/java/curso/udemy/spring/angular/ionc/CargaInput.java
@@ -73,6 +73,11 @@ public class CargaInput implements CommandLineRunner {
     private void inserirCategoriasEProdutos() {
         Categoria cat1 = new Categoria(null, "Informática");
         Categoria cat2 = new Categoria(null, "Escritório");
+        Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+        Categoria cat4 = new Categoria(null, "Eletrônicos");
+        Categoria cat5 = new Categoria(null, "Jardinagem");
+        Categoria cat6 = new Categoria(null, "Decoração");
+        Categoria cat7 = new Categoria(null, "Perfumaria");
 
         produto1 = new Produto(null, "Computador", 2000.00);
         produto2 = new Produto(null, "Impressora", 800.00);
@@ -85,7 +90,7 @@ public class CargaInput implements CommandLineRunner {
         produto2.getCategorias().addAll(Arrays.asList(cat1, cat2));
         produto3.getCategorias().addAll(Arrays.asList(cat1));
 
-        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
         produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
     }
 
--- src/main/java/curso/udemy/spring/angular/ionc/resources/CategoriaResource.java
+++ src/main/java/curso/udemy/spring/angular/ionc/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import curso.udemy.spring.angular.ionc.domain.Categoria;
 import curso.udemy.spring.angular.ionc.dto.CategoriaDTO;
 import curso.udemy.spring.angular.ionc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -51,5 +52,16 @@ public class CategoriaResource {
         List<Categoria> categorias = service.findAll();
         List<CategoriaDTO> categoriasDto = categorias.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(categoriasDto);
+    }
+
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    public ResponseEntity<Page<CategoriaDTO>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC")  String direction) {
+        Page<Categoria> categorias = service.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoriaDTO> categoriasDTO = categorias.map(obj -> new CategoriaDTO((obj)));
+        return ResponseEntity.ok().body(categoriasDTO);
     }
 }
