351c8ffc01950f1f148ecd43af3aeb938ba59d4b
hugoiguana <hugo.iguanaa@gmail.com>
2019-06-04 22:08:16 -0300

    Ajuste nos profiles de Test e Dev; Ignorando arquivos de log;

---
+++ src/main/java/curso/udemy/spring/angular/ionc/config/DevConfig.java
@@ -0,0 +1,30 @@
+package curso.udemy.spring.angular.ionc.config;
+
+import java.text.ParseException;
+
+import curso.udemy.spring.angular.ionc.services.DBService.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        if (!"create".equals(strategy) || !"create-drop".equals(strategy)) {
+            return false;
+        }
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
---
+++ src/main/java/curso/udemy/spring/angular/ionc/config/TestConfig.java
@@ -0,0 +1,23 @@
+package curso.udemy.spring.angular.ionc.config;
+
+import java.text.ParseException;
+
+import curso.udemy.spring.angular.ionc.services.DBService.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
