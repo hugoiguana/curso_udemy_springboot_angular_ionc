fafeceacc83bd35341e9823fd1250761120b4e20
hugoiguana <hugo.iguanaa@gmail.com>
2019-06-10 20:50:39 -0300

    Salvando perfis de usuario na base de dados

--- src/main/java/curso/udemy/spring/angular/ionc/domain/Cliente.java
+++ src/main/java/curso/udemy/spring/angular/ionc/domain/Cliente.java
@@ -3,6 +3,7 @@ package curso.udemy.spring.angular.ionc.domain;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import curso.udemy.spring.angular.ionc.domain.enums.Perfil;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.*;
 
@@ -51,14 +53,32 @@ public class Cliente  implements Serializable {
     @OneToMany(mappedBy = "cliente")
     private List<Pedido> pedidos = new ArrayList();
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable( name = "perfis" )
+    private Set<Integer> perfis = new HashSet<>();
+
+
     public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
         super();
         this.id = id;
         this.nome = nome;
         this.email = email;
         this.cpfOuCnpj = cpfOuCnpj;
-        this.tipo = (tipo==null) ? null : tipo.getCod();
+        this.tipo = (tipo == null) ? null : tipo.getCod();
         this.senha = senha;
+    }
+
+    @PrePersist
+    public void prePersiste() {
+        addPerfil(Perfil.CLIENTE);
+    }
+
+    public Set<Perfil> getPerfis() {
+        return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addPerfil(Perfil perfil) {
+        perfis.add(perfil.getCod());
     }
 
     @Override
