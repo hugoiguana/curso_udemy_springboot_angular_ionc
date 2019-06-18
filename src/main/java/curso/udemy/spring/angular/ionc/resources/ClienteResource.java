04666a8a66195f0636326578eac46521ee6d59ee
hugoiguana <hugo.iguanaa@gmail.com>
2019-06-17 22:15:16 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/curso/udemy/spring/angular/ionc/resources/ClienteResource.java
+++ src/main/java/curso/udemy/spring/angular/ionc/resources/ClienteResource.java
@@ -7,6 +7,7 @@ import curso.udemy.spring.angular.ionc.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -28,6 +29,7 @@ public class ClienteResource {
         return ResponseEntity.ok().body(cliente);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<ClienteDTO>> findAll() {
         List<Cliente> clientes = service.findAll();
@@ -35,6 +37,7 @@ public class ClienteResource {
         return ResponseEntity.ok().body(clienteDTOList);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/page", method = RequestMethod.GET)
     public ResponseEntity<Page<ClienteDTO>> findPage(
             @RequestParam(value = "page", defaultValue = "0") Integer page,
@@ -64,6 +67,7 @@ public class ClienteResource {
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         service.delete(id);
