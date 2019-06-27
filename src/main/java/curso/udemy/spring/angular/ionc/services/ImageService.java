7febc15aed9d11c1f90cf906693dc819c626be5a
hugoiguana <hugo.iguanaa@gmail.com>
2019-06-26 21:39:04 -0300

    Bonus: ajustando tamanho da imagem

--- src/main/java/curso/udemy/spring/angular/ionc/services/ImageService.java
+++ src/main/java/curso/udemy/spring/angular/ionc/services/ImageService.java
@@ -13,7 +13,7 @@ import curso.udemy.spring.angular.ionc.services.exceptions.FileException;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
+import org.imgscalr.Scalr;
 
 @Service
 public class ImageService {
@@ -48,5 +48,19 @@ public class ImageService {
         } catch (IOException e) {
             throw new FileException("Erro ao ler arquivo");
         }
+    }
+
+    public BufferedImage cropSquare(BufferedImage sourceImg) {
+        int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth();
+        return Scalr.crop(
+                sourceImg,
+                (sourceImg.getWidth() / 2) - (min / 2),
+                (sourceImg.getHeight() / 2) - (min / 2),
+                min,
+                min);
+    }
+
+    public BufferedImage resize(BufferedImage sourceImg, int size) {
+        return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
     }
 }
