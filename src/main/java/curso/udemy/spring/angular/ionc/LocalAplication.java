b7163ee987f98b44756053dfd5a3fc5b38174318
hugoiguana <hugo.iguanaa@gmail.com>
2019-06-26 21:16:43 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/curso/udemy/spring/angular/ionc/LocalAplication.java
+++ src/main/java/curso/udemy/spring/angular/ionc/LocalAplication.java
@@ -1,13 +1,21 @@
 package curso.udemy.spring.angular.ionc;
 
+import curso.udemy.spring.angular.ionc.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LocalAplication {
 
+    @Autowired
+    private S3Service s3Service;
+
     public static void main(String[] args) {
         SpringApplication.run(LocalAplication.class, args);
     }
 
+    public void run(String... args) throws Exception {
+        s3Service.uploadFile("C:\\temp\\fotos\\ana.jpg");
+    }
 }
